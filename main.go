376c4e57b3c4851aaa31eae07ee5985dc0736cdc
Neil Cowie <thecowster@hotmail.co.uk>
2025-03-15 12:43:02 +0000

    Hands-on exercise #41 - Use a slice literal to store string elements in a slice

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 /*
-Hands-on exercise #40
-	● Use an array literal to store these elements in an array and let the compiler determine
-	the length of the array, then also print out the array and the length of the array
+Hands-on exercise #41
+	● Use a slice literal to store these elements in a slice, then also print out the slice and the
+		length of the slice.
+	● if you can, try to get a "for range" loop working on the slice
+		○ you can reference the documentation for a "for range" loop here:
+			■ https://go.dev/doc/effective_go#for
 */
 
 package main
@@ -10,7 +13,7 @@ import "fmt"
 
 func main() {
 
-	ice_creams := [...]string{
+	ice_creams := []string{
 		"Bourbon Turtle (GF)", "Browned Butter Cookie Dough", "Chocolate Covered Black Cherry (GF)", "Chocolate Fudge Brownie",
 		"Almond Biscotti Café", "Banana Pudding ", "Balsamic Strawberry (GF)", "Bittersweet Chocolate (GF)", "Blueberry Pancake (GF)",
 		"Chocolate Peanut Butter (GF)", "Coffee (GF)", "Cookies & Cream", "Fresh Basil (GF)", "Garden Mint Chip (GF)",
@@ -23,4 +26,8 @@ func main() {
 	fmt.Println(ice_creams)
 	fmt.Printf("size of ice_creams is %v\n", len(ice_creams))
 	fmt.Printf("type of ice_creams is %T\n", ice_creams)
+
+	for idx, value := range ice_creams {
+		fmt.Printf("%v - %v\n", idx, value)
+	}
 }
