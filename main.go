6050c2e64c9cd0546bad8f523ef8cf7e7cd76276
Neil Cowie <thecowster@hotmail.co.uk>
2025-03-16 08:23:06 +0000

    Hands-on exercise #43 - composite literal slice and usage

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 /*
-Hands-on exercise #42
+Hands-on exercise #43
 	● Using a COMPOSITE LITERAL:
-		○ create an ARRAY which holds 5 VALUES of TYPE int
-		○ assign VALUES to each index position.
-	● Range over the array and print the values out.
+	● create a SLICE of TYPE int
+	● assign these 10 VALUES
+		42, 43, 44, 45, 46, 47, 48, 49, 50, 51
+	● Range over the slice and print the values out.
 		○ print out the VALUE and the TYPE
 */
 
@@ -13,16 +14,9 @@ import "fmt"
 
 func main() {
 
-	ai := [5]int{}
+	ai := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
 
-	ai[0] = 100
-	ai[1] = 101
-	ai[2] = 102
-	ai[3] = 103
-	ai[4] = 104
-
-	fmt.Println(ai)
-	fmt.Printf("%T\n", ai)
+	fmt.Printf("%#v\n", ai)
 	for idx, val := range ai {
 		fmt.Printf("index = %v \t type = %T \t value = %v\n", idx, val, val)
 	}
