1d866ad909c4ff7b7d470d15b381669f61a2b702
Neil Cowie <thecowster@hotmail.co.uk>
2025-03-16 13:13:28 +0000

    Hands-on exercise #45 - appending slices

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 /*
-Hands-on exercise #44
-	Using the code from the previous example, use SLICING to create the following new slices
-	which are then printed:
-		● [42 43 44 45 46]
-		● [47 48 49 50 51]
-		● [44 45 46 47 48]
-		● [43 44 45 46 47]
+Hands-on exercise #45
+Follow these steps:
+	● start with this slice
+		○ x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	● append to that slice this value
+		○ 52
+	● print out the slice
+	● in ONE STATEMENT append to that slice these values
+		○ 53
+		○ 54
+		○ 55
+	● print out the slice
+	● append to the slice this slice
+		○ y := []int{56, 57, 58, 59, 60}
+	● print out the slice
 */
 
 package main
@@ -14,18 +22,17 @@ import "fmt"
 
 func main() {
 
-	ai := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	fmt.Printf("%v\n", x)
 
-	s1 := ai[:5]
-	fmt.Printf("%#v\n", s1)
+	x = append(x, 52)
+	fmt.Printf("%v\n", x)
 
-	s2 := ai[5:]
-	fmt.Printf("%#v\n", s2)
+	x = append(x, 53, 54, 55)
+	fmt.Printf("%v\n", x)
 
-	s3 := ai[2:7]
-	fmt.Printf("%#v\n", s3)
-
-	s4 := ai[1:6]
-	fmt.Printf("%#v\n", s4)
+	y := []int{56, 57, 58, 59, 60}
+	x = append(x, y...)
+	fmt.Printf("%v\n", x)
 
 }
